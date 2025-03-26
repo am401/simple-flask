b95eb5a28b9bfbefe261d7644d64aeaf3ec9f73f
am401 <54643953+am401@users.noreply.github.com>
2025-03-26 07:49:44 -0500

    Update to shell script

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 yum update -y
-amazon-linux-extras install epel -y
+# No longer available
+# amazon-linux-extras install epel -y
 yum install nginx -y
 yum install git -y
 yum install gcc -y
 yum install build-essential -y
 yum install python3-pip python3-devel python3-setuptools -y
 
-git config --system credential.https://git-codecommit.us-east-1.amazonaws.com.helper '!aws --profile default codecommit credential-helper $@'
-git config --system credential.https://git-codecommit.us-east-1.amazonaws.com.UseHttpPath true
+git config --global user.name "am401"
+
+# Setup ssh config for github
+
+#echo "Host github.com\n\tHostName github.com\n\tPreferredAuthentications publickey\n\tIdentityFile ~/.ssh/file" >> ~/.ssh/config
 
 aws configure set region us-east-1
 
 mkdir -p /var/www
 
 # update me
-git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/Simple-Flask-Autoscaling /var/www
+git clone https://github.com/am401/simple-flask.git /var/www
 
 cd /var/www
 
 git config core.fileMode false
 
 # update me
-aws s3 cp s3://tci-s3-demo/simple-flask/.env .env
+aws s3 cp s3://tci-s3-demo-032125/simple-flask/.env .env
 
 chmod +x scripts/post_userdata.sh
 
